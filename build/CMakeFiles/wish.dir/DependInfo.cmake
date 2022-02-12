2012f11934236165bf96b00f5a30e9fc6123542d
leeyyi <leeyyi@vip.qq.com>
2022-02-12 20:08:00 +0800

    batch_mode

---
+++ build/CMakeFiles/wish.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/liyuyi/shell_proj/src/batch.c" "/home/liyuyi/shell_proj/build/CMakeFiles/wish.dir/src/batch.c.o"
+  "/home/liyuyi/shell_proj/src/parser.c" "/home/liyuyi/shell_proj/build/CMakeFiles/wish.dir/src/parser.c.o"
+  "/home/liyuyi/shell_proj/src/wish.c" "/home/liyuyi/shell_proj/build/CMakeFiles/wish.dir/src/wish.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/wish.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wish.dir/src/batch.c.o"
+  "CMakeFiles/wish.dir/src/parser.c.o"
+  "CMakeFiles/wish.dir/src/wish.c.o"
+  "wish"
+  "wish.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/wish.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
