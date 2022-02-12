3178593f83421ac1a8d2a40c580405d268e9671c
leeyyi <leeyyi@vip.qq.com>
2022-02-12 20:09:05 +0800

    batch_mode

---
+++ include/batch.h
@@ -0,0 +1,1 @@
+void BATCH(const char*);
---
+++ src/batch.c
@@ -0,0 +1,32 @@
+#include"batch.h"
+#include"parser.h"
+#include"constants.h"
+#include<unistd.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+FILE*FP=NULL;
+void BATCH(const char* argv){
+    FILE* fp = fopen(argv,"r");
+    FP=fp;
+    if(fp==NULL){
+        char error_message[30] = "An error has occurred\n";
+        write(STDERR_FILENO, error_message, strlen(error_message));
+        exit(1);
+    }
+    char*str=NULL;
+    size_t size=0;
+    int getline_code;
+    while((getline_code=getline(&str,&size,fp))!=-1){
+        //注意getline会读入换行符
+        if(str[strlen(str)-1]=='\n')
+        str[strlen(str)-1]='\0';
+        insSet* myIns = parser(str);
+        if(exec(myIns)==END_LOOP)
+        break;
+        printf("%s\n",str);
+        free(str);
+        str=NULL;
+        size_t size=0;
+    }
+}
